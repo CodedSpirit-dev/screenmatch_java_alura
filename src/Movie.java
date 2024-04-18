fc5af6111f2e85223a769d5857918bdd0aa2d7b6
CodedSpirit <viradvt@gmail.com>
2024-04-17 19:25:05 -0600

    Frist commit

---
+++ src/Movie.java
@@ -0,0 +1,6 @@
+public class Movie {
+    String name;
+    int year;
+    int lengthInMinutes;
+    Boolean includedInTheUserPlan;
+}
---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Movie myMovie;
+        myMovie = new Movie();
+        myMovie.name = "The Matrix";
+        myMovie.year = 1999;
+        myMovie.lengthInMinutes = 136;
+        myMovie.includedInTheUserPlan = true;
+    }
+}
