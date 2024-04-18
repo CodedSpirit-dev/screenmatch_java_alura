565c9325895697a5b65b541da3d558a22e7efcf8
CodedSpirit <viradvt@gmail.com>
2024-04-17 19:29:05 -0600

    First commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,23 @@
 public class Principal {
     public static void main(String[] args) {
-        Movie myMovie;
-        myMovie = new Movie();
-        myMovie.name = "The Matrix";
-        myMovie.year = 1999;
-        myMovie.lengthInMinutes = 136;
-        myMovie.includedInTheUserPlan = true;
+        Movie TheMatrix;
+        TheMatrix = new Movie();
+        TheMatrix.name = "The Matrix";
+        TheMatrix.year = 1999;
+        TheMatrix.lengthInMinutes = 136;
+        TheMatrix.includedInTheUserPlan = true;
+
+        System.out.println("My movie is " + TheMatrix.name);
+        System.out.println("It was released in " + TheMatrix.year);
+        System.out.println("It lasts " + TheMatrix.lengthInMinutes + " minutes");
+        System.out.println("Is it included in the user plan? " + TheMatrix.includedInTheUserPlan);
+
+        Movie TheMatrixReloaded;
+        TheMatrixReloaded = new Movie();
+        TheMatrixReloaded.name = "The Matrix Reloaded";
+        TheMatrixReloaded.year = 2003;
+        TheMatrixReloaded.lengthInMinutes = 138;
+        TheMatrixReloaded.includedInTheUserPlan = false;
+
     }
 }
