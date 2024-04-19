7c562e15fc2d7337fabb65b7b356ee4e5ec63bd3
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-04-18 18:45:08 -0600

    show info

--- src/Movie.java
+++ src/Movie.java
@@ -3,4 +3,11 @@ public class Movie {
     int year;
     int lengthInMinutes;
     Boolean includedInTheUserPlan;
+
+    void showMovieInfo() {
+        System.out.println("My movie is " + name);
+        System.out.println("It was released in " + year);
+        System.out.println("It lasts " + lengthInMinutes + " minutes");
+        System.out.println("Is it included in the user plan? " + includedInTheUserPlan);
+    }
 }
