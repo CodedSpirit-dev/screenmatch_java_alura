f12f6eb7a103dc7553d37c4010097b20008ca97e
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-04-29 19:03:08 -0600

    Adding Jackson and Gson

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,21 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ApiConsume;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumeApi = new ApiConsume();
+		var json = consumeApi.obtainData("https://www.omdbapi.com/?t=game+of+thrones&apikey=6c660fbc");
+		System.out.println(json);
+	}
 }
